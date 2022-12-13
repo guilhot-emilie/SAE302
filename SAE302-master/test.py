0ca1e9c648195b428773e51a02a03f2a0647ba6b
emili <emilie.guilhot@uha.fr>
2022-12-13 16:57:40 +0100

    master

--- SAE302-master/test.py
+++ SAE302-master/test.py
@@ -1,17 +1,20 @@
-import platform, cpuinfo
+import platform, cpuinfo, psutil
+from socket import *
 #télécharger py-cpuinfo
 #psutils
 
 print("="*40, "System Information", "="*40)
 uname = platform.uname()
-print(f"System: {uname.system}")
-print(f"Node Name: {uname.node}")
+print(f"OS: {uname.system} {uname.version}")
+print(f"Nom du pc: {uname.node}")
+print ("adresse IP",gethostbyname(gethostname()))
 
-print("="*40)
-print(f"Release: {uname.release}")
-print(f"Version: {uname.version}")
+print("="*40, "TEST", '='*40)
+
 print(f"Machine: {uname.machine}")
 print(f"Processor: {uname.processor}")
-
-
 print(f"cpu info: {cpuinfo.get_cpu_info()}")
+
+print(psutil.virtual_memory())
+
+
