701b074abcab8b034d6e2548afaa6babe67bc2d9
emili <emilie.guilhot@uha.fr>
2022-12-14 14:41:18 +0100

    master

--- SAE302-master/test.py
+++ SAE302-master/test.py
@@ -1,6 +1,5 @@
-import platform, cpuinfo, psutil
+import platform, psutil
 from socket import *
-#télécharger py-cpuinfo
 
 uname = platform.uname()
 print("="*40, "System Information", "="*40)
@@ -10,8 +9,6 @@ print(f"Nom du pc: {uname.node}")
 print ("adresse IP",gethostbyname(gethostname()))
 print("cpu pourcentage:",psutil.cpu_percent(1),"%")
 print("RAM totale:",round(psutil.virtual_memory().total / (1024.0 ** 3), 2),"GB")
+print("RAM utilisée:", round(psutil.virtual_memory().used / (1024.0 ** 3), 2), "GB")
+print("RAM libre:", round(psutil.virtual_memory().free / (1024.0 ** 3), 2), "GB")
 
-
-print("="*40, "TEST", '='*40)
-
-memorytotal = psutil.virtual_memory().total / (1024.0 ** 3)
