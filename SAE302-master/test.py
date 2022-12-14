2ab8454d4f24b23f2d3bd9bd55d7f477205a2c78
emili <emilie.guilhot@uha.fr>
2022-12-14 14:23:35 +0100

    master

--- SAE302-master/test.py
+++ SAE302-master/test.py
@@ -9,10 +9,9 @@ print(f"OS: {uname.system} {uname.version}")
 print(f"Nom du pc: {uname.node}")
 print ("adresse IP",gethostbyname(gethostname()))
 print("cpu pourcentage:",psutil.cpu_percent(1),"%")
+print("RAM totale:",round(psutil.virtual_memory().total / (1024.0 ** 3), 2),"GB")
+
 
 print("="*40, "TEST", '='*40)
 
-print(f"Processor: {uname.processor}")
-print(f"cpu info: {cpuinfo.get_cpu_info()}")
-
-print(psutil.virtual_memory())
+memorytotal = psutil.virtual_memory().total / (1024.0 ** 3)
